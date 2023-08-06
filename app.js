cd30393251df9c6f4dbe0774f8ccf8d5f27827e8
Yonatan Kostov <jonikostov@gmail.com>
2023-08-06 21:29:37 +0300

    project 12

---
+++ app.js
@@ -0,0 +1,16 @@
+const { PORT = 3000 } = process.env;
+const express = require("express");
+
+const app = express();
+const userRouter = require("./routes/users");
+const cardRouter = require("./routes/cards");
+
+app.use("/users", userRouter);
+app.use("/cards", cardRouter);
+app.use((req, res) => {
+  res.status(404).send({ message: "The requested resource was not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
